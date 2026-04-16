72700ff665be7f0abdd3ff8c88b0b7083c68e709
eliseo23p <225574400+Eliseo23P@users.noreply.github.com>
2026-04-16 01:04:23 +0000

    use la implementacion de las utils, para conectar planificar con cpu e io

--- io/src/main.c
+++ io/src/main.c
@@ -1,6 +1,28 @@
-#include <utils/hello.h>
+#include <utils/sockets.h>
 
 int main(int argc, char* argv[]) {
-    saludar("io");
+
+
+        int conexion = crear_conexion("127.0.0.1", "9098");
+  
+    int numero = 0;
+
+
+    while(numero != 5) {
+        printf("\nEscribí un número , el es 5 para salir): ");
+        scanf("%d", &numero); 
+
+        if (numero == 5) {
+            break; 
+        }
+
+        send(conexion, &numero, sizeof(int), 0);
+
+        printf("Enviado al servidor: %d\n", numero);
+    }
+   
+    liberar_conexion(conexion);
+    printf("Conexión terminada.\n");
+    
     return 0;
 }
